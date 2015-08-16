d0dc4428b5db2f5ffb1c39d7552732a13e8478e3
Nguyen Duc Tri <ductriuv@gmail.com>
2015-08-16 23:45:28 +0700

    Get detail of class

--- src/hcmut/tutorclub/controller/ClassManagerController.java
+++ src/hcmut/tutorclub/controller/ClassManagerController.java
@@ -76,7 +76,6 @@ public class ClassManagerController implements IClassManagerController {
         	List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
         	WorksheetEntry worksheetEntry = null;
         	for (WorksheetEntry worksheet:worksheets) {
-        		System.out.println(worksheet.getTitle().getPlainText());
         		if (worksheet.getTitle().getPlainText().equals(WORKSHEET_NAME)) {
         			worksheetEntry = worksheet;
         			break;
